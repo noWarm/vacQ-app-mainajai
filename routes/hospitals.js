89750c729b46820185cb309c96b0ffbdc6b9e1b9
hacchaa <jomwit22@gmail.com>
2023-01-30 16:02:00 +0700

    refactor: created hospital controller

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,24 +1,8 @@
 const express = require('express');
+const { getAllHospital, createHospital, getHospital, updateHospital, deleteHospital } = require('../controllers/hospitals');
 router = express.Router();
 
-router.get('/', (req,res) => {
-    res.status(200).json({success:true, msg:'Show all hospitals'});
-});
-
-router.get('/:id', (req,res) => {
-    res.status(200).json({success:true, msg:`Show hospital ${req.params.id}`});
-});
-
-router.post('/', (req,res) => {
-    res.status(200).json({success:true, msg:'Create new hospitals'});
-});
-
-router.put('/:id', (req,res) => {
-    res.status(200).json({success:true, msg:`Update hospital ${req.params.id}`});
-});
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({success:true, msg:`Delete hospital ${req.params.id}`});
-});
+router.route('/').get(getAllHospital).post(createHospital);
+router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
 
 module.exports=router;

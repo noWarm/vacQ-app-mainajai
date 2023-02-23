e372286f71d06c0af39a8ceb107b21a1702afc0d
noWarm <jomwit22@gmail.com>
2023-02-23 17:52:52 +0700

    feat: add a middleware layer that validates the cookie first

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,80 +1,85 @@
- const User = require('../models/User');
+const User = require("../models/User");
 
- exports.register = async (req, res, next) => {
-    try {
-        // recall that we use the json parser
-        const {name, email, password, role} = req.body;
+exports.register = async (req, res, next) => {
+  try {
+    // recall that we use the json parser
+    const { name, email, password, role } = req.body;
 
-        // create the user
-        const user = await User.create({
-            name,
-            email,
-            password,
-            role
-        });
+    // create the user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+    });
 
-        // create the token and return it too
-        // const token = user.getSignedJwtToken();
-        // res.status(200).json({success:true, token});
-
-        sendTokenResponse(user, 200, res);s
-
-
-    } catch(err) {
-        res.status(400).json({success:false});
-        console.log(err.stack);
-    }
- }
-
- exports.login = async (req, res, next) => {
-    const { email, password } = req.body;
-    // Validate email & password
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ success: false, msg: "Please provide email and password" });
-    }
-    // Check for user
-    const user = await User.findOne({ email }).select("+password");
-    if (!user) {
-      return res.status(400).json({ success: false, msg: "Invalid credentials" });
-    }
-    // Check if password matches
-    const isMatch = await user.matchPassword(password);
-    if (!isMatch) {
-      return res.status(400).json({ success: false, msg: "Invalid credentials" });
-    }
-
-     // create the token and return it too
-    //  const token = user.getSignedJwtToken();
-    //  res.status(200).json({success:true, token});
+    // create the token and return it too
+    // const token = user.getSignedJwtToken();
+    // res.status(200).json({success:true, token});
 
     sendTokenResponse(user, 200, res);
-  };
+    s;
+  } catch (err) {
+    res.status(400).json({ success: false });
+    console.log(err.stack);
+  }
+};
 
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  // Validate email & password
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide email and password" });
+  }
+  // Check for user
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(400).json({ success: false, msg: "Invalid credentials" });
+  }
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return res.status(400).json({ success: false, msg: "Invalid credentials" });
+  }
+
+  // create the token and return it too
+  //  const token = user.getSignedJwtToken();
+  //  res.status(200).json({success:true, token});
+
+  sendTokenResponse(user, 200, res);
+};
 
 // This function is wrapper for the previous
 // naive getSignedJwtToken + res.status(200,...);
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
-    // Create token
-    const token = user.getSignedJwtToken();
-  
-    const options = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // millisecond
-      ),
-      httpOnly: true,
-    };
-  
+  // Create token
+  const token = user.getSignedJwtToken();
 
-
-
-    if (process.env.NODE_ENV === "production") {
-      options.secure = true; //TODO: why is this!?!?
-    }
-    res
-      .status(statusCode)
-      .cookie("token", token, options)
-      .json({ success: true, token });
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // millisecond
+    ),
+    httpOnly: true,
   };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true; //TODO: why is this!?!?
+  }
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, token });
+};
+
+
+//TODO: who call this getMe and why ?? 
+exports.getMe = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+};
--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getAllHospital, createHospital, getHospital, updateHospital, deleteHospital } = require('../controllers/hospitals');
+const { protect } = require('../middleware/auth');
 router = express.Router();
 
-router.route('/').get(getAllHospital).post(createHospital);
-router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
+router.route('/').get(getAllHospital).post(protect, createHospital);
+router.route('/:id').get(getHospital).put(protect, updateHospital).delete(protect, deleteHospital);
 
 module.exports=router;

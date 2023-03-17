cacdda33ba316af34278f6d45f1bb3755f4424c1
noWarm <jomwit22@gmail.com>
2023-03-17 13:08:08 +0700

    feat: can now add appointment

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,4 +1,5 @@
 const Appointment = require("../models/Appointment");
+const Hospital = require("../models/Hospital");
 
 // @desc    Get all appointments
 // @route   GET /api/v1/.../appointments
@@ -7,7 +8,7 @@ exports.getAppointments = async (req, res, next) => {
   let query;
 
   let findParams = {};
-  
+
   // if the method is being used in a nested url of /hospitals/:hospitalId/appointments
   if (req.params.hospitalId) {
     findParams.hospital = req.params.hospitalId;
@@ -20,8 +21,8 @@ exports.getAppointments = async (req, res, next) => {
 
   // we'll add more fields to the hospital field, turning the query document into a nested object
   query = Appointment.find(findParams).populate({
-    path: 'hospital',
-    select: 'name province tel',
+    path: "hospital",
+    select: "name province tel",
   });
 
   try {
@@ -46,12 +47,15 @@ exports.getAppointments = async (req, res, next) => {
 exports.getAppointmentById = async (req, res, next) => {
   try {
     const appointment = await Appointment.findById(req.params.id).populate({
-      path: 'hospital',
-      select: 'name description tel',
+      path: "hospital",
+      select: "name description tel",
     });
 
     if (!appointment) {
-      return res.status(404).json({success:false, message: `No appointment with the id of ${req.params.id}`})
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${req.params.id}`,
+      });
     }
 
     res.status(200).json({
@@ -66,4 +70,35 @@ exports.getAppointmentById = async (req, res, next) => {
   }
 };
 
+// @desc    Add appointment
+// @route   POST /api/v1/hospitals/:hospitalId/appointment
+// @access  Private
+exports.addAppointment = async (req, res, next) => {
+  try {
+    // we require that the handler is used at the specified route
+    // and not POST /api/v1/appointment (will be 404)
 
+    // add the params into the payload
+    req.body.hospital = req.params.hospitalId;
+
+    const hospital = await Hospital.findById(req.params.hospitalId);
+
+    if (!hospital) {
+      return res.status(404).json({
+        success: false,
+        message: `No hospital with the id of ${req.params.hospitalId}`,
+      });
+    }
+
+    const appointment = await Appointment.create(req.body); // pass the payload into the model
+    res.status(200).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Cannot create Appointment" });
+  }
+};
--- routes/appointments.js
+++ routes/appointments.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   getAppointments,
   getAppointmentById,
+  addAppointment,
 } = require("../controllers/appointments");
 
 const router = express.Router({ mergeParams: true }); // Preserve the req.params values from the parent router. If the parent and the child have conflicting param names, the child’s value take precedence.
@@ -9,8 +10,7 @@ const router = express.Router({ mergeParams: true }); // Preserve the req.params
 
 const { protect } = require("../middleware/auth");
 
-router.route("/").get(protect, getAppointments);
+router.route("/").get(protect, getAppointments).post(protect, addAppointment);
 router.route("/:id").get(protect, getAppointmentById);
-
 
 module.exports = router;

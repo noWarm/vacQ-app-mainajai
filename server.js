677bfc47c4f943905f4a5622a88d1451538bec46
noWarm <jomwit22@gmail.com>
2023-04-01 11:34:40 +0700

    add swagger package

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
@@ -31,9 +33,24 @@ app.use(xss());
 //Rate Limiting
 const limiter = rateLimit({
   windowsMs: 10 * 60 * 1000, //10 mins
-  max: 1,
+  max: 100,
 });
 app.use(limiter);
+
+// use swagger so that it shows the api routes
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 // Mount the routers
 // important that you mount the router after you used all other npm packages
